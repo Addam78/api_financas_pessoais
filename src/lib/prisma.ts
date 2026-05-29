e5ef1ac207fe4b8795fbb48dd17e081ec20c7bd0
Addam Cosmo <addamcosmo2000@gmail.com>
2026-05-29 20:18:19 -0300

    fix: remove credenciais comentadas do prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,15 +3,6 @@ import pg from 'pg'
 import { PrismaClient } from '../generated/prisma/client'
 
 
-
-// const pool = new pg.Pool({
-//   host: 'localhost',
-//   port: 5432,
-//   user: 'postgres',
-//   password: 'gyhkil987%',
-//   database: 'api_financas',
-// })
-
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 })
